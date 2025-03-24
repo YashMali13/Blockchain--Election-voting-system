566f9a6772059aa980a6cbd29f452398a86ce276
YashMali13 <it.a.63.yashmali@gmail.com>
2025-03-24 19:43:12 +0530

    Blockchain-Based Election Voting System

---
+++ lib/utils/constants.dart
@@ -0,0 +1,18 @@
+String infura_url = "https://sepolia.infura.io/v3/1bfee29640e044899cae1d5873c5d26b";
+    // "https://kovan.infura.io/v3/7e0fcba16fce454c96616d04abdbf3a4";
+
+
+String owner_private_key =
+    "8cabb42faf442bd178b977ccaa1b565b067d3cc9c7cbe0f7d24d4894277861b8";
+
+String voter_private_key =
+    // "24b2aa19e93fbd217db7e6de86e2b77b546f87ccfb0521f6377f876053e485b4";
+      "a0de7ecc222a7776179f546bfbb66b594a03f3e5b7d4db97e36741eb6b52db3f";
+
+
+String contractAddress1 = "0x8d14BCC5AA8D626CD1AED1286AF637c3768c3D5A";
+
+
+
+// 1bfee29640e044899cae1d5873c5d26b   my first key
+// 0x8d14bcc5aa8d626cd1aed1286af637c3768c3d5a cont ID
